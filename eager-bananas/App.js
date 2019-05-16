f0d768badbb51f4e4bb63f2204972ef18998e527
isabelraposo <isabelrraposo@gmail.com>
2019-05-16 17:53:59 +0100

    removed shadow

--- eager-bananas/App.js
+++ eager-bananas/App.js
@@ -3,7 +3,7 @@ import { Text, View, StyleSheet } from 'react-native';
 import { Constants } from 'expo';
 
 // You can import from local files
-import AssetExample from './components/AssetExample';
+import QRbutton from './components/QRbutton';
 
 // or any pure javascript modules available in npm
 import { Card } from 'react-native-paper';
@@ -12,12 +12,12 @@ export default class App extends React.Component {
   render() {
     return (
       <View style={styles.container}>
-        <Text style={styles.paragraph}>
-          Change code in the editor and watch  jsmilddod it change on your phone! Save to get a shareable url.
-        </Text>
-        <Card>
-          <AssetExample />
+        <Card style={styles.card}>
+          <QRbutton />
         </Card>
+        <Text style={styles.paragraph}>
+          click for knowledge
+        </Text>
       </View>
     );
   }
@@ -28,13 +28,23 @@ const styles = StyleSheet.create({
     flex: 1,
     justifyContent: 'center',
     paddingTop: Constants.statusBarHeight,
-    backgroundColor: '#ecf0f1',
-    padding: 8,
+    backgroundColor: '#fa8072',
+    padding: 8
   },
   paragraph: {
     margin: 24,
-    fontSize: 18,
+    fontSize: 30,
     fontWeight: 'bold',
     textAlign: 'center',
+    color: '#fff',
   },
+  card: {
+    backgroundColor: '#fa8072',
+    alignItems: 'center',
+    justifyContent: 'center',
+    shadowColor: null,
+    shadowOffset: null,
+    shadowOpacity: null,
+    elevation: null
+  }
 });
---
+++ eager-bananas/components/QRbutton.js
@@ -0,0 +1,32 @@
+import * as React from 'react';
+import { Text, View, StyleSheet, Image } from 'react-native';
+
+export default class QRbutton extends React.Component {
+  render() {
+    return (
+      <View style={styles.view}>
+        <Image style={styles.logo} source={require('../assets/logo.png')} />
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  
+  logo: {
+    alignItems: 'center',
+    justifyContent: 'center',
+    backgroundColor: '#fa8072',
+    height: 184,
+    width: 135,
+    borderRadius: 100
+  },
+  view: {
+    alignItems: 'center',
+    justifyContent: 'center',
+    backgroundColor: '#fa8072',
+    height: 200,
+    width: 200,
+    borderRadius: 100
+  }
+});
